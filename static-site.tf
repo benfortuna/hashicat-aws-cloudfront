f961959a3658520d0e894b562cb51e89d760e7ab
Ben Fortuna <ben.fortuna@team.telstra.com>
2022-03-03 13:57:03 +1100

    Upgrade to latest module version

--- static-site.tf
+++ static-site.tf
@@ -1,6 +1,6 @@
 module "static-site" {
   source  = "app.terraform.io/benfortuna_tfc/static-site/aws"
-  version = "1.0.1"
+  version = "1.0.3"
   # insert required variables here
   bucket_name = "hashicat-static"
   domain = null
